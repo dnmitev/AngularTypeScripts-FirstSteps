1e19b53e140e92a9bae815b7232b976a999ec7a5
dnmitev <dimitar.mitev@bulpros.com>
2016-01-22 11:11:07 +0200

    Product Management System with TS and Angular

---
+++ APM/app/products/productDetailCtrl.ts
@@ -0,0 +1,33 @@
+module app.productDetail {
+    interface IProductDetailModel {
+        title: string;
+        product: app.domain.IProduct;
+    }
+    
+    interface IProductParams extends ng.route.IRouteParamsService {
+        productId: number;
+    }
+
+    class ProductDetailCtrl implements IProductDetailModel {
+        title: string;
+        product: app.domain.IProduct;
+        
+        static $inject = ["$routeParams", "dataAccessService"];
+        constructor(
+            private $routeParams: IProductParams,
+            private dataAccessService: app.common.DataAccessService) {
+            this.title = "Product Detail";
+            
+            var id = $routeParams.productId;
+            var productResponse = dataAccessService.getProductResource();
+            productResponse.get({productId: id},
+                (data: app.domain.IProduct) => {
+                this.product = data;
+            })
+        }
+    }
+   
+    angular
+        .module("productManagement")
+        .controller("ProductDetailCtrl", ProductDetailCtrl);
+}
